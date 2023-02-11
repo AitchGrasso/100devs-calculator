e999d4f49c2a6fa46c1ba6dbd162c38138d0090b
H Grasso <aitch.grasso@gmail.com>
2023-02-11 00:16:57 -0800

    changes

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,8 @@
 
+document.querySelector('button').addEventListener('click', display)
+
+function display(){
+    document.querySelector('#display').innerHTML = "7"
+    console.log("hi, I work?")
+}
+
