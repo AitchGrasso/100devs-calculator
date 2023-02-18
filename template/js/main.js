5b6c82a39a387c978aa638589bcaa8bb45130751
H Grasso <aitch.grasso@gmail.com>
2023-02-17 19:35:27 -0800

    2/17 edit

--- template/js/main.js
+++ template/js/main.js
@@ -1,8 +1,20 @@
 
-document.querySelector('button').addEventListener('click', display)
 
-function display(){
-    document.querySelector('#display').innerHTML = "7"
-    console.log("hi, I work?")
+// let display = document.querySelector('#display').innerHTML
+
+let buttons = document.querySelectorAll('.btn')
+buttons.forEach(ele => ele.addEventListener('click', buttonPress))
+console.log(buttons)
+
+function buttonPress(Event){
+    let display = []
+    let value = Event.target.attributes[0].value
+    console.log(value)
+    display = display.push(value)
+    console.log(display)
+    document.querySelector('#display').innerHTML = display
+    // display = display.concat(value)
+    // document.querySelector('#display').innerHTML = ${Event.}
+    // console.log('HI, imdoing something right')
+    console.log(Event)
 }
-
