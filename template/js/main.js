4b4d760b41c0d44c7421166d4cbb130f7f712354
H Grasso <aitch.grasso@gmail.com>
2023-02-22 22:24:22 -0600

    constructor addition

--- template/js/main.js
+++ template/js/main.js
@@ -6,15 +6,47 @@ let buttons = document.querySelectorAll('.btn')
 buttons.forEach(ele => ele.addEventListener('click', buttonPress))
 console.log(buttons)
 
-function buttonPress(Event){
-    let display = []
-    let value = Event.target.attributes[0].value
-    console.log(value)
-    display = display.push(value)
-    console.log(display)
-    document.querySelector('#display').innerHTML = display
-    // display = display.concat(value)
-    // document.querySelector('#display').innerHTML = ${Event.}
-    // console.log('HI, imdoing something right')
-    console.log(Event)
+// function buttonPress(Event){
+//     let display = []
+//     let value = Event.target.attributes[0].value
+//     console.log(value)
+//     display = display.push(value)
+//     console.log(display)
+//     document.querySelector('#display').innerHTML = display
+//     // display = display.concat(value)
+//     // document.querySelector('#display').innerHTML = ${Event.}
+//     // console.log('HI, imdoing something right')
+//     console.log(Event)
+// }
+
+class Calculator {
+    constructor(firstArg, secondArg, operationSymbol) {
+        this.first = firstArg
+        this.second = secondArg
+        this.operation = operationSymbol     
+    }
+    add(){
+        return this.first + this.second
+    }
+    
+     subtract(){
+        return this.first - this.second
+    }
+    
+     multiply(){
+        return this.first * this.second
+    }
+    
+     divide(){
+        return this.first / this.second
+    }
 }
+
+// + ascii 43
+// - ascii 45
+// * ascii 42
+// / ascii 47
+// = ascii 61
+// . ascii 46
+
+let sessionOne = new Calculator(12, 15, '+')
